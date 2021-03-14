fce0a9f90e1c6071d76612e04568cbbea571a7f9
uwue <blazeisnmn@gmail.com>
2021-03-14 03:37:41 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from pypresence import Presence
+import time
+import random
+import ctypes
+import json
+import requests
+
+ctypes.windll.kernel32.SetConsoleTitleW("CatRPC by uwue")
+
+# Set the Client ID
+cid = int(820573529438289940)
+rpc = Presence(cid)
+rpc.connect()
+
+cats = (1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14)
+
+# RPC Loop
+while True:
+
+    data = requests.get('https://catfact.ninja/fact')
+    cat_fact = data.json()["fact"]
+    random_cat = random.choice(cats)
+    rpc.update(small_image = 'small', large_image = str(random_cat), small_text= 'another cat!', large_text= str(cat_fact)[:128])
+    print('RPC set successfully.')
+    time.sleep(10)
