0f87bc8584bd84d1ceccab107b9c6c6418396779
uwue <78624182+uwue@users.noreply.github.com>
2021-03-14 04:27:06 -0700

    Update main.py
    epic!!!

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ while True:
     data = requests.get('https://catfact.ninja/fact')
     cat_fact = data.json()["fact"]
     random_cat = random.choice(cats)
-    rpc.update(small_image = 'small', large_image = str(random_cat), small_text= 'another cat!', large_text= str(cat_fact)[:128])
+    rpc.update(small_image = 'cat', large_image = str(random_cat), small_text= 'another cat!', large_text= str(cat_fact)[:128])
     print('Looped ' + str(loops) + ' times!')
     loops = loops+1
     time.sleep(10)
